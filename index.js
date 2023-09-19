3e2d9d37d502366d7b8ef10ff62fa8b57367f6e7
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-09-19 17:36:05 +0800

    add functionality

--- index.js
+++ index.js
@@ -1,22 +1,94 @@
 const cheerio = require("cheerio")
 const axios = require("axios")
-
-const url = "https://expense-tracker-henna-three.vercel.app/"
+const fs = require("fs")
+const ExcelJS = require("exceljs")
+const readline = require("readline")
 
 const main = async () => {
-  try {
-    const headers = {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-      // Add other headers as needed
+  let url = ""
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  rl.question("Search Job: ", async function (userInput) {
+    url = `https://www.jobstreet.com.ph/${userInput}-jobs`
+
+    try {
+      const { data: html } = await axios.get(url)
+      const $ = cheerio.load(html)
+      const obj = []
+
+      // job titles
+      $(".z1s6m00 > h1 > a > div > span").each((index, element) => {
+        obj.push({ title: $(element).text() })
+      })
+
+      // company
+      $("a[data-automation=jobCardCompanyLink]").each((index, element) => {
+        const jobObj = obj[index]
+        obj[index] = { ...jobObj, company: $(element).text() }
+      })
+
+      // address
+      $("a[data-automation=jobCardLocationLink]").each((index, element) => {
+        const jobObj = obj[index]
+        obj[index] = { ...jobObj, location: $(element).text() }
+      })
+      // console.log(obj)
+
+      fs.writeFile("job-data.json", JSON.stringify(obj), (err) => {
+        if (err) throw err
+        console.log("Job scraped successfully!")
+      })
+
+      // Creating an Excel workbook and worksheet
+      const workbook = new ExcelJS.Workbook()
+      const worksheet = workbook.addWorksheet("Jobs")
+
+      // Adding headers to the worksheet
+      worksheet.columns = [
+        { header: "Title", key: "title", width: 60 },
+        { header: "Company", key: "company", width: 50 },
+        { header: "Location", key: "location", width: 30 },
+      ]
+
+      // Adding the scraped data to the worksheet
+      obj.forEach((job) => {
+        worksheet.addRow(job)
+      })
+
+      // Saving the workbook to a file
+      await workbook.xlsx.writeFile("jobs.xlsx")
+      console.log("Excel file has been created successfully.")
+    } catch (error) {
+      console.error("Error:", error.message)
     }
 
-    const { data: html } = await axios.get(url)
-    const $ = cheerio.load(html)
-    console.log($("h2").text())
-  } catch (error) {
-    console.error("Error:", error.message)
-  }
+    // rl.close()
+
+
+
+    rl.question("Do you want to open the Excel file (y/n)? ", (answer) => {
+      if (answer.toLowerCase() === "y") {
+        rl.close();
+        // Check if the file exists before attempting to open it
+        if (fs.existsSync("jobs.xlsx")) {
+          const { exec } = require("child_process");
+          exec(`start ${"jobs.xlsx"}`, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`Error opening the file: ${error}`);
+            }
+          });
+        } else {
+          console.error(`The file '${"jobs.xlsx"}' does not exist.`);
+        }
+      } else {
+        rl.close();
+      }
+    });
+  })
 }
 
 main()
